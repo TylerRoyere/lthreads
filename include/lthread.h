add307b5e4f955fd18d3d2e337cdae09361dd65b
Tyler Royere <teroyere@gmail.com>
2021-11-06 15:09:17 -0400

    This is a better interface I think

--- include/lthread.h
+++ include/lthread.h
@@ -13,23 +13,46 @@ enum lthread_status {
     DONE,
 };
 
-struct lthread {
-    void *(*start_routine)(void *data);
-    void *data;
-    enum lthread_status status;
-    ucontext_t context;
-    void *stack;
-    size_t id;
-    struct lthread *next;
+struct lthread_info {
+    void *(*start_routine)(void *data); /* Thread entry point */
+    void *data; /* Data passed to entry point, return value */
+    enum lthread_status status; /* Scheduling status of thread */
+    ucontext_t context; /* Stored context of thread */
+    void *stack; /* Pointer to end of the stack */
+    size_t id; /* Allocated ID for the thread */
+    struct lthread_info *next; /* Next thread in the queue */
 #ifdef LTHREAD_DEBUG
+    /* Debug information to valgrind stops complaining */
     unsigned int stack_reg;
 #endif
 };
 
+/* Thread handle will be its ID */
+typedef size_t lthread;
 
+/* Start scheduling lthreads */
 int lthread_init(void);
-int lthread_create(struct lthread *t, void *data, void *(*start_routine)(void *data));
-int lthread_join(struct lthread *t, void **retval);
-void lthread_destroy(struct lthread *t);
+
+/* Create an lthread 't' whose execution will start at 
+ * the specified entry point 'start_routine'
+ *
+ * The start routing will be passed 'data' as the one
+ * and only parameter to 'start_routine'
+ *
+ * When 'start_routine' completes, the return value will
+ * be saved for retrieval by a subsquent call to lthread_join
+ */
+int lthread_create(lthread *t, void *(*start_routine)(void *data), void *data);
+
+/* Waits for a thread 't' to complete execution. The return value of
+ * that instance of 'start_routine' will be saved in 'retval' if
+ * 'retval' is not NULL
+ */
+int lthread_join(lthread t, void **retval);
+
+/* Stops a thread of executing in a more desructive fashion, the return
+ * value is not recorded
+ */
+void lthread_destroy(lthread t);
 
 #endif
